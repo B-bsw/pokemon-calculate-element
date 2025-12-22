087f7633a1270013b6fc54389bbccad04c870311
B-bsw <sorawitthassadorn15@gmail.com>
2025-12-22 20:33:26 +0700

    fix: design table

--- src/app/pokemon/elements/page.tsx
+++ src/app/pokemon/elements/page.tsx
@@ -2,7 +2,7 @@ import TablePokemon from '@/components/table/TablePokemon'
 
 const Page = () => {
     return (
-        <div className="mx-4 pt-10">
+        <div className="mx-4 pt-10 h-screen flex items-end">
             <TablePokemon />
         </div>
     )
